978379b676fe9424a28a1014c8e3b1f0d838ac1f
Subham777-max <subhamray865@gmail.com>
2026-05-16 20:51:41 +0530

    feat: implement AppError class for enhanced error handling

---
+++ backend/src/utils/errors/AppError.js
@@ -0,0 +1,23 @@
+/**
+ * @description A custom error class that extends the built-in Error class to include additional properties such as status code and operational status.
+ * @class AppError
+ * @extends Error
+ */
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+
+    this.status =
+      statusCode >= 400 && statusCode < 500
+        ? "fail"
+        : "error";
+
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default AppError;
